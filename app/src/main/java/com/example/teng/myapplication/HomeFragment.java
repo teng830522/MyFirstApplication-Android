d391c2d86e27e41a0f6bdd7a147f0586160f1a69
Teng <teng830522@yahoo.com.tw>
2018-10-04 12:01:22 +0800

    VideoView

--- app/src/main/java/com/example/teng/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/teng/myapplication/HomeFragment.java
@@ -1,12 +1,10 @@
 package com.example.teng.myapplication;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/com/example/teng/myapplication/LatestNews.java
+++ app/src/main/java/com/example/teng/myapplication/LatestNews.java
@@ -1,16 +1,23 @@
 package com.example.teng.myapplication;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class LatestNews extends android.support.v4.app.Fragment {
 
     private View view ;
-
+    VideoView videoView ;
+    MediaController controller ;
+    int position ;
 
     @Nullable
     @Override
@@ -22,8 +29,23 @@ public class LatestNews extends android.support.v4.app.Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
+        setVideoView();
     }
 
-
+    private void setVideoView(){
+        videoView = view.findViewById(R.id.videoView);
+        controller = new MediaController(getActivity());
+        videoView.setMediaController(controller);
+        Uri uri = Uri.parse("android.resource://" + getActivity().getPackageName() + "/" + R.raw.black_hole);
+        videoView.setVideoURI(uri);
+//        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener(){
+//
+//            @Override
+//            public void onPrepared(MediaPlayer mp) {
+//                if(position == 0){
+//                    mp.start();
+//                }
+//            }
+//        });
+    }
 }
